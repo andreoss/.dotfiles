8cef325c57634f23098ccfee272e1aff28b04d8a
a <andreoss@sdf.org>
2016-01-05 21:17:12 -0500

    post-install script

---
+++ misc/get-latest-alpine.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use v5.14;
+
+use strict;
+use warnings;
+use feature qw(say);
+
+use HTTP::Tiny;
+use CPAN::Meta::YAML;
+
+our $base_url =
+  'https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64';
+
+our $client = HTTP::Tiny->new;
+
+my $response = $client->get("$base_url/latest-releases.yaml");
+
+die("Failed to fetch data: $response->{reason}\n")
+  unless ( $response->{success} );
+
+my $yaml = CPAN::Meta::YAML->read_string( $response->{content} )
+  or die( CPAN::Meta::YAML->errstr );
+
+my $prefered_flavour = shift || 'alpine-virt';
+for my $x ( map { @$_ } @$yaml ) {
+    if ( $x->{flavor} eq $prefered_flavour ) {
+        say "${base_url}/" . $x->{iso};
+        exit(0);
+    }
+}
+exit(1);
