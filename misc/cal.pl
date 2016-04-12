3a0e487a6816e26864b8ccd61d4f1019ab1fcd51
a <andreoss@sdf.org>
2016-04-12 03:57:09 -0400

    fix day time issue

--- misc/cal.pl
+++ misc/cal.pl
@@ -1,75 +1,87 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use feature 'say';
+
 use Time::Piece ':override';
 use Time::Seconds;
 
+use experimental qw(signatures);
+use feature      qw(say);
+
 BEGIN {
-    Time::Piece->use_locale()
+    Time::Piece->use_locale();
 }
 
+sub boxed($n) { sprintf( ">%3s", int($n) ); }
 
-sub boxed {
-    sprintf(">%3s", int(shift));
+sub color( $day, $color_code ) {
+    return "\${color $color_code}$day\${color}";
 }
 
-sub color {
-    my ($day, $color_code) = @_;
-    return "\${color $color_code}$day\${color}";  
+sub centered($text) {
+    return "\${alignc}$text";    # Centered format
 }
 
-sub centered {
-    my $text = shift;
-    return "\${alignc}$text";  # Centered format
+sub grayed_out($text) {
+    color( $text, 'grey' );
 }
-
-sub grayed_out {
-    color(shift, 'grey');
-}
-
 
 our $today = localtime;
 
-sub wday {shift->wday;}
+sub wday($d) { $d->wday; }
 
-my $f = $today; 
+my $f = $today;
 while (1) {
     $f -= ONE_DAY;
-    next if $f->mon  == $today->mon;
+    next if $f->mon == $today->mon;
     next if $f->wday != 1;
     last;
 }
 
-my @t= ();
-my @w= ();
+my @t = ();
+my @w = ();
 
-push(@t, [centered($today->fullmonth)]);
-push(@t, [map {sprintf("%4s",$_)} localtime->day_list()]);
-for (my $c = $f; !($c->mon > $today->mon && wday($c) == 1); $c += ONE_DAY ) {
+push( @t, [ centered( $today->fullmonth ) ] );
+push( @t, [ map { sprintf( "%4s", $_ ) } localtime->day_list() ] );
 
-    my $o = sprintf("%4s", $c->mday);  
+sub next_day($c) {
+    my $n = $c + ONE_DAY;
+    if ( $c->mday == $n->mday ) {
+        $n += 60 * 60;
+    }
+    $n;
+}
 
-    if ($c->mon != $today->mon) {
+for (
+    my $c = $f ;
+    !( $c->mon > $today->mon && wday($c) == 1 ) ;
+    $c = next_day($c)
+  )
+{
+
+    my $o = sprintf( "%4s", $c->mday );
+
+    if ( $c->mon != $today->mon ) {
         $o = grayed_out($o);
     }
-    if ($c->mday == $today->mday && $c->mon == $today->mon) {
+    if ( $c->mday == $today->mday && $c->mon == $today->mon ) {
         $o = boxed($o);
     }
-    if (wday($c) == 1 && $c->mon == $today->mon) {
-        $o = color($o, 'ea3333');
+    if ( wday($c) == 1 && $c->mon == $today->mon ) {
+        $o = color( $o, 'ea3333' );
     }
-    if (wday($c) == 7 && $c->mon == $today->mon) {
-        $o = color($o, 'aa3333');
-    } 
+    if ( wday($c) == 7 && $c->mon == $today->mon ) {
+        $o = color( $o, 'aa3333' );
+    }
+    push( @w, $o );
 
-    push(@w, $o);
-    
-    if (wday($c) == 7) {
-        push(@t, [@w]);
+    if ( wday($c) == 7 ) {
+        push( @t, [@w] );
         @w = ();
     }
 }
 
-say "\${alignc}${_}" for map {join(' ', map { sprintf("%s", $_) } @$_)} @t;
+say "\${alignc}${_}" for map {
+    join( ' ', map { sprintf( "%s", $_ ) } @$_ )
+} @t;
 
